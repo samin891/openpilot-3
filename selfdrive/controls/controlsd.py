056ff1e2e7440c45ccd9e939d9442f9f8b94ef76
multikyd <72329880+multikyd@users.noreply.github.com>
2021-09-13 08:42:27 +0900

    kyd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -497,7 +497,7 @@ class Controls:
     if not self.joystick_mode:
       # accel PID loop
       pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, self.v_cruise_kph * CV.KPH_TO_MS)
-      actuators.accel = self.LoC.update(self.active and CS.cruiseAccStatus, CS, self.CP, long_plan, pid_accel_limits, self.sm['radarState'])
+      actuators.accel = self.LoC.update(self.active and CS.cruiseState.speed > 1., CS, self.CP, long_plan, pid_accel_limits, self.sm['radarState'])
 
       # Steering PID loop and lateral MPC
       desired_curvature, desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,
