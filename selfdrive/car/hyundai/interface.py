39be723f499739e533e5dea252f32824e0ba6ea0
multikyd <72329880+multikyd@users.noreply.github.com>
2021-09-15 16:25:08 +0900

    long: tune test

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -62,10 +62,13 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = True
 
-    ret.longitudinalTuning.kpBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kpV = [1.2, 1.0, 0.8, 0.65, 0.5, 0.4]
-    ret.longitudinalTuning.kiBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kiV = [0.3, 0.24, 0.22, 0.18, 0.15, 0.13]
+    ret.longitudinalTuning.kpV = [0.5]
+    ret.longitudinalTuning.kiV = [0.1]
+
+    #ret.longitudinalTuning.kpBP = [0., 4., 9., 17., 23., 31.]
+    #ret.longitudinalTuning.kpV = [1.2, 1.0, 0.8, 0.65, 0.5, 0.4]
+    #ret.longitudinalTuning.kiBP = [0., 4., 9., 17., 23., 31.]
+    #ret.longitudinalTuning.kiV = [0.3, 0.24, 0.22, 0.18, 0.15, 0.13]
 
     ret.longitudinalTuning.deadzoneBP = [0., 4.]
     ret.longitudinalTuning.deadzoneV = [0., 0.1]
