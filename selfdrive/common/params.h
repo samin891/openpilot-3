4d9df3929c9246fc0b7c78708da539e0b59bf288
hikee9123 <hikee9123@gmail.com>
2021-09-22 22:58:00 +0900

    fix

--- selfdrive/common/params.h
+++ selfdrive/common/params.h
@@ -80,4 +80,14 @@ public:
 
 private:
   const std::string params_path;
+
+
+public:  // atom
+  inline int getInt( const char *key )
+  {
+    int   ret_code = 0;
+    std::string result = get( key );
+    ret_code = std::stoi( result );
+    return ret_code;
+  }
 };
