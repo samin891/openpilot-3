4bdc0a2ddc2b4d1c16e554f31c8d4e0701423d2a
multikyd <72329880+multikyd@users.noreply.github.com>
2021-09-30 00:19:30 +0900

    kyd

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -35,7 +35,7 @@ CURRENT_TAU = 15.   # 15s time constant
 TEMP_TAU = 5.   # 5s time constant
 DAYS_NO_CONNECTIVITY_MAX = 7  # do not allow to engage after a week without internet
 DAYS_NO_CONNECTIVITY_PROMPT = 4  # send an offroad prompt after 4 days with no internet
-DISCONNECT_TIMEOUT = 5.  # wait 5 seconds before going offroad after disconnect so you get an alert
+DISCONNECT_TIMEOUT = 3.  # wait 5 seconds before going offroad after disconnect so you get an alert
 
 ThermalBand = namedtuple("ThermalBand", ['min_temp', 'max_temp'])
 
